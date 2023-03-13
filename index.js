1efd61a125b288a815772ef295f2b42b258cbf95
Michael Wood <michaelwood33311@icloud.com>
2023-03-13 14:36:51 -0400

    added detecting whether file is valid csv and parsing csv if valid

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const fs = require('fs');
 
 const formidable = require('formidable');
 const mysql = require('mysql2');
+const csvParse = require('csv-parse');
 
 const config = {
  host: '127.0.0.1',
@@ -26,29 +27,10 @@ console.log(config)
 
 const pool = mysql.createPool(config);
 
-pool.query("SHOW DATABASES", function(err, rows, fields) {
-   if (err) return console.error(err);
-    console.log(rows);
-})
-
-// pool.getConnection((err, connection) => {
-//   if(err)
-//   throw err;
-//   console.log('MYSQL connected successfully');
-//   connection.release();
-// });
-
-// const query = query => {
-//   return new Promise((resolve, reject) => {
-//     pool.query(query, (err, data) => {
-//       if(err) {
-//         reject(err);
-//         return;
-//       }
-//       resolve (data);
-//     })
-//   })
-// }
+// pool.query("SHOW DATABASES", function(err, rows, fields) {
+//    if (err) return console.error(err);
+//     console.log(rows);
+// })
 
 const app = express();
 app.use(express.static('public'));
@@ -63,8 +45,18 @@ app.post('/csv', (req, res) => {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, data) {
         if (err) return console.error(err);
-        console.log(data['File[]'].filepath);
-        console.log(fields.chart);
+        const fileName = data['File[]'].filepath;
+        const chart = fields.chart;
+
+        const input = fs.readFileSync(fileName, "utf-8");
+        csvParse.parse(input, (err, records) => {
+            if (err) {
+                res.status(401).json({err});
+                return console.error(err);
+            }
+            console.log(records);
+        })
+
         // const filePath = files.filetoupload.filepath;
         // console.log('files stored in', filePath);
         res.write('File uploaded');
