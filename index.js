9415bf946cd989d121b995115523529c9060178a
Michael Wood <michaelwood33311@icloud.com>
2023-03-13 14:23:34 -0400

    added mysql connection to instant_charts table

--- index.js
+++ index.js
@@ -9,6 +9,46 @@ const cors = require('cors');
 const fs = require('fs');
 
 const formidable = require('formidable');
+const mysql = require('mysql2');
+
+const config = {
+ host: '127.0.0.1',
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0
+}
+console.log(config)
+
+const pool = mysql.createPool(config);
+
+pool.query("SHOW DATABASES", function(err, rows, fields) {
+   if (err) return console.error(err);
+    console.log(rows);
+})
+
+// pool.getConnection((err, connection) => {
+//   if(err)
+//   throw err;
+//   console.log('MYSQL connected successfully');
+//   connection.release();
+// });
+
+// const query = query => {
+//   return new Promise((resolve, reject) => {
+//     pool.query(query, (err, data) => {
+//       if(err) {
+//         reject(err);
+//         return;
+//       }
+//       resolve (data);
+//     })
+//   })
+// }
 
 const app = express();
 app.use(express.static('public'));
